b0ae2022cb6870a7ebc84b43c0d38a72b005772d
filipporaciti <filipporaciti15@libero.it>
2023-05-28 15:39:19 +0200

    make 3

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { UpdateComponent } from './update/update.component';
+import { ShowComponent } from './show/show.component';
+
+const routes: Routes = [
+
+  { path: 'add', component: AddComponent },
+  { path: 'update', component: UpdateComponent },
+  { path: 'show', component: ShowComponent },
+  { path: '**', component: ShowComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { UpdateComponent } from './update/update.component';
+import { ShowComponent } from './show/show.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    UpdateComponent,
+    ShowComponent
   ],
   imports: [
     BrowserModule,
