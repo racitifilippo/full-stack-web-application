dbc457c4646ff88388b3e683364e19b5696f216d
racitifilippo <raciti.filippo@iisgalvanimi.edu.it>
2023-05-29 12:56:10 +0000

    make 4

--- frontend/src/app/show/show.component.ts
+++ frontend/src/app/show/show.component.ts
@@ -7,20 +7,40 @@ import { Component } from '@angular/core';
 })
 export class ShowComponent {
 
-  deleteCondition: boolean = true
+  //deleteCondition: boolean = true
 
   items = [
     {
+      'id': 0,
       'First': 'Mark',
       'Last': 'Otto',
       'Handle': '@mdo',
+      'deleteCondition': true
   },
   {
+    'id': 1,
     'First': 'Marco',
     'Last': 'Rossi',
     'Handle': '@mrssi',
+    'deleteCondition': true
 }
   ]
+
+
+
+
+
+
+
+
+  
+delete_button(id: number, set: boolean) {
+  for (let x of this.items) {
+    if (x['id'] == id){
+      x['deleteCondition'] = set
+    }
+  }
+}
 
 
 }
